2272f9c8a119950b1b6374c648115e2c8f9b1b3a
Jum-eng <jmusah007@st.ug.edu.gh>
2025-03-08 21:53:26 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyttsx3 as p
 
 engine = p.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate',130)
+print(rate)
 
 engine.say("hello world, my name is Jumah")
 engine.runAndWait()
